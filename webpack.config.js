aed9aef6fffb40e7b3d4b289ddaba38776cd001f
Robert <demond611@gmail.com>
2017-06-18 22:24:34 -0500

    moving files from travel site

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path'); // NEEDED FOR USING INSTALLED WEBPACK MODULE CORRECTLY
+
+module.exports = {
+	entry: {
+		App: './app/assets/scripts/app.js',
+		Vendor: './app/assets/scripts/Vendor.js'
+	},
+	output: {
+		path: path.resolve(__dirname, "./app/temp/scripts"),
+		filename: "[name].js"
+	},
+	module: {
+		loaders: [
+			{
+				loader: 'babel-loader',
+				query: {
+					presets: ['es2015']
+				},
+				test: /\.js$/,
+				exclude: /node_modules/
+			}
+		]
+	}
+}
