9a0dd612a754fb88f268de91669b7b3f0a2b6e98
Robert <demond611@gmail.com>
2017-06-18 23:21:58 -0500

    final moves/changes to travel site

--- app/assets/scripts/Vendor.js
+++ app/assets/scripts/Vendor.js
@@ -2,4 +2,6 @@
 // BEST PRACTICE IS TO PUT THEM IN THEIR OWN .JS FILE
 // IMAGE FILES NEED ALT ATTRIBUTE FOR 'lazysizes' TO WORK CORRECTLY
 
+import '../../temp/scripts/modernizr';
+import 'picturefill';
 import 'lazysizes';
